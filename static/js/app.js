19050e3c660fa2ae480db9fa40ff97c1dc3625bb
Bmatz0729 <bmartinez0729@gmail.com>
2019-11-20 23:12:31 -0500

    updating app.py file

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
+var $tbody = document.querySelector("tbody");
+
+
 // YOUR CODE HERE!
+function LoadTable() {
+  
+  for (var i = 0; i < tableData.length; i++) {
+    
+
+    var ufo = tableData[i];
+    var sighting = Object.keys(ufo);
+    
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < sighting.length; j++) {
+      
+      var ufo_sighting = sighting[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = ufo[ufo_sighting];
+    }
+  }
+}
+
+
+
+// Render the table for the first time on page load
+LoadTable();
